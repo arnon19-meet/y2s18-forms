53da3243d061749785c60c6a86a339791cd69931
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 18:12:41 +0300

    g

--- exercises/app.py
+++ exercises/app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', students=query_all())
 
+@app.route('/add', methods=['GET','POST'])
+def add_student_route():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        return "Received POST request", render_template('add.html')
+
+
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
